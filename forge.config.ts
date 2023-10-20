c6aaf2214ad41e40cde2daed8eaabac9a8eb5591
Liubin <liubinMac@qq.com>
2023-10-20 12:15:04 +0800

    fix

--- forge.config.ts
+++ forge.config.ts
@@ -22,7 +22,7 @@ const config: ForgeConfig = {
     {
       name: '@electron-forge/publisher-github',
       config:{
-        authToken: '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',
+        // authToken: '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',
         repository: {
           owner: 'liu-liubin',
           name: 'electron-ts'
